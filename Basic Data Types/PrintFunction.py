8cce09711d4c7f92705a9fbe9ddbfa3daa97eab8
meshreyas24 <42927270+meshreyas24@users.noreply.github.com>
2020-08-03 17:51:10 +0530

    Add files via upload
    Python Print() Function Operation

---
+++ Basic Data Types/PrintFunction.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("This is how we print on screen")
+print() #To add Space 
+print("What just happened")
+print("This is message on one line.", end ="")#Print on same line
+print("This is a message on same line.")
+
+print("Python is Fun!")
+
+
