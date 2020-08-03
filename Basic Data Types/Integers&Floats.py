2e48ea8ef2756dd58b9ad8a4bfd8ab92254c7433
meshreyas24 <42927270+meshreyas24@users.noreply.github.com>
2020-08-03 19:30:40 +0530

    Add files via upload
    Integers and Floats

---
+++ Basic Data Types/Integers&Floats.py
@@ -0,0 +1,26 @@
+print("5") #String
+print(5) #Integer
+
+print(4+2) #add
+print(4-2) #subtract
+print(4*2) #multiply
+print(4/2) #divide
+print(4**2) #for squaring
+print (4%2) #remainder zero after dividing
+
+y = 4.15
+print(y)
+
+
+y = y + 2 # add 2 into y
+print(y)
+
+
+y += 2 # add 2 into y
+print(y)
+
+y = y - 1 # subtract 1 into y
+print(y)
+
+y -= 1 # subtract 1 into y
+print(y)
