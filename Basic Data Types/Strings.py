886d3d3899a25a91eadeeb396f2d76d498367b7d
meshreyas24 <42927270+meshreyas24@users.noreply.github.com>
2020-08-03 18:20:51 +0530

    Add files via upload
    String Functions

---
+++ Basic Data Types/Strings.py
@@ -0,0 +1,22 @@
+#strings
+
+print("Hello World!")
+
+full_name = "Jonny Smith"
+print(full_name) #It will print complete name
+
+print(full_name.upper())#It will print in Upper Case
+print(full_name.lower())#It will print in lower Case
+
+print(full_name.title())#Title Format #Starting Alphabet In Uppercase
+
+print(full_name.strip())#It will Strip It
+
+print(full_name)
+full_name = full_name.title().strip()
+print(full_name)
+
+
+
+
+
