d9a9753e406d75f5a7d836ddd27b12028baf2a4f
meshreyas24 <42927270+meshreyas24@users.noreply.github.com>
2020-08-03 18:01:22 +0530

    Add files via upload
    Variable Introduction

---
+++ Basic Data Types/Variables.py
@@ -0,0 +1,18 @@
+#variable intro
+
+x = 5
+
+print(5)
+print(x) #it will print value of x
+print("x") #it will print x
+
+message = "Hello World!" #String
+print(message)
+
+
+message = "Good Bye Cruel World!" #String
+print(message)
+
+#bad idea to overide builtin function
+#print = "Happy"
+#Sprint(print)
